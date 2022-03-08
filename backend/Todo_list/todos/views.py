e494ca63822b345773a48b971834094f54b3bea3
samadbek1998 <88585307+samadbek1998@users.noreply.github.com>
2022-03-08 15:35:44 +0500

    todo_app
    Bu dasturda biz o'zimizni bajaroqchi bo'lgan ishlarrimizni hammasini yozib qo'shib qoysak bo'ladi. VA bundan shunday imkoniyat qilinganki siz bemalol bajarmoqchi bo'lgan ishlaringizni qo'shib ketsangiz bo'ldi.  Agar o'zgartirmoqchi bo'lsangiz bemalol xoxlagan paytizda o'zgartirish imkoniyati bor.

---
+++ backend/Todo_list/todos/views.py
@@ -0,0 +1,12 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Todo
+from .serializers import TodoSerializer
+
+class ListTodo(ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
